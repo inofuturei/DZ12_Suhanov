a4978e26e0bdc8e9a1bf4bcb42d1e1f81f00323d
inofuturei <vip.artemik@mail.ru>
2024-12-20 22:44:46 +0300

    Добавьте файлы проекта.

---
+++ ProgramModuleHW12/Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace HelloDocker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // 5 комментарий для проверки ci cd
+            Console.WriteLine("Hello, Docker!");
+        }
+    }
+}
